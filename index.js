6b2196c09cf77f37288eb2bcd952254a635844ff
francesc.perez <francbonet@gmail.com>
2017-05-25 18:10:44 +0200

    initial server functions

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //Server
+
+var schedule = require('node-schedule');
+var Servidor = require('./lib/server');
+
+
+/*Init server http port 3000 */
+/*Nose*/ 
+
+var servidor = new Servidor();
+servidor.arrancar(  __dirname );
